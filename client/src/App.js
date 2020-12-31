002700d45db3f6dbcf970e677f5e6b8b913ab415
Lashange166 <bankteeradhol@gmail.com>
2020-12-31 07:22:20 -0800

    last commit for 2020 and some new bug

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ function App() {
         <MainContextProvider>
           <Routes />
           <Footer />
-          <Toast />
+          <Toast />   
         </MainContextProvider>
       </Router>
   );
