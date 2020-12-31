2b010f5ec44fe3dd84edfde91d4e0cfefc700531
Lashange166 <bankteeradhol@gmail.com>
2020-12-31 02:58:21 -0800

    .

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongosoe = require("mongoose");
 const cookieParser = require("cookie-parser");
 const cors = require('cors');
-
+const path = require('path')
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -23,7 +23,14 @@ app.use('/user', require('./routes/userRoute'));
 app.use('/product', require('./routes/productRoute'));
 app.use('/order', require('./routes/ordersRoute'));
 
+if(process.env.NODE_ENV=="production"){
+    app.use(express.static("client/build"))
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+    })
+}
 
-const PORT = process.env.PORT;
+
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => console.log('server ' + PORT))
